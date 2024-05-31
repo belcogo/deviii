4f43c266c03e63237c744cf76aa886feaf2e5465
BrunoHoffmann15 <bruno.siqueira.hoffmann@gmail.com>
2024-05-31 20:55:43 -0300

    feat(boolk): add book class

--- library-api/src/main/java/com/unisinos/library/model/Book.java
+++ library-api/src/main/java/com/unisinos/library/model/Book.java
@@ -1,5 +1,32 @@
 package com.unisinos.library.model;
 
-public class Book {
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String title;
+
+    @Column(nullable = false)
+    public String author;
+
+    @Column(nullable = false)
+    public LocalDate publishedDate;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    public ZonedDateTime createdDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    public User owner;
 }
