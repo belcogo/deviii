7fca26fecd4dfb62b87b3296bdab1413b74b3d8f
belcogo <bel.cogo27@gmail.com>
2024-07-05 17:13:23 -0300

    fix: ajuste comportamento user deslogado

--- responsive-library/src/App.js
+++ responsive-library/src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useNavigate } from 'react-router-dom'
 import { CreateBookPage, LoginPage, MyBooksPage, RegisterPage, ProfilePage, SearchPage } from './pages';
 import { FooterTab } from './components/footer-tab/footer-tab.component';
 import { Header } from './components/header/header.component';
 import { Loader } from './components';
 import { Modal } from './components/modal/modal.component';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from './state/user.atom';
+import { useEffect } from 'react';
 
-const Element = ({ children, showFooter }) => (
-  <>
-    <Header />
-    {children}
-    {showFooter && <FooterTab />}
-    <Loader />
-    <Modal/>
-  </>
-)
+const Element = ({ children, showFooter }) => {
+  const navigate = useNavigate()
+  const user = useRecoilValue(userAtom)
+
+  useEffect(() => {
+    if (!user?.id) navigate('/login')
+  }, [navigate, user])
+  
+  return (
+    <>
+      <Header />
+      {children}
+      {showFooter && <FooterTab />}
+      <Loader />
+      <Modal/>
+    </>
+  )
+}
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/'>
