e6b5c8caa3c79ace50c8f38a1198b394126a6773
belcogo <bel.cogo27@gmail.com>
2024-07-05 10:51:22 -0300

    feat: adiciona tela de busca

--- responsive-library/src/components/card-list/card-list.component.js
+++ responsive-library/src/components/card-list/card-list.component.js
@@ -1,9 +1,10 @@
 import { Card } from '../card/card.component'
+import './card-list.style.css'
 
-export function CardList({ books }) {
+export function CardList({ books, showIcon }) {
   return (
-    <div className="pageContent">
-      {books?.map(({ title, genre }, idx) => <Card key={`${title} ${idx}`} title={title} subtitle={genre?.name} showIcon />)}
+    <div className="pageContent list">
+      {books?.map(({ title, genre }, idx) => <Card key={`${title} ${idx}`} title={title} subtitle={genre?.name} showIcon={showIcon} />)}
     </div>
   )
 }
--- responsive-library/src/pages/search/search.page.js
+++ responsive-library/src/pages/search/search.page.js
@@ -1,16 +1,44 @@
 import { useRecoilValue } from "recoil";
 import { othersBooksAtom } from "../../state/books.atom";
-import { CardList } from "../../components";
-import { useNavigate } from 'react-router-dom';
+import { CardList, Input } from "../../components";
+import { useEffect, useMemo, useState } from "react";
+
+function checkFilters(title, author, genre, term) {
+	const titleToCompare = title?.toLowerCase()
+	const authorToCompare = author?.toLowerCase()
+	const genreToCompare = genre?.toLowerCase()
+	const termToCompare = term?.toLowerCase()
+
+	return (titleToCompare?.includes(termToCompare)
+		|| authorToCompare.includes(termToCompare)
+		|| genreToCompare?.includes(termToCompare))
+}
 
 export function SearchPage() {
+	const [searchTerm, setSearchTerm] = useState('')
+	const [filterTerm, setFilterTerm] = useState('')
 	const othersBooks = useRecoilValue(othersBooksAtom)
-	const navigate = useNavigate();
-	const handlePress = () => navigate('/books/create')
+	const booksToShow = useMemo(() => {
+		if (!filterTerm) return othersBooks
+		return othersBooks?.filter(({ title, author, genre}) => {
+			return checkFilters(title, author, genre.name, filterTerm)
+		})
+	}, [othersBooks, filterTerm])
+
+	const handleChange = (e) => setSearchTerm(e?.target?.value)
+
+	useEffect(() => {
+		const timeout = setTimeout(() => setFilterTerm(searchTerm), 1500)
+
+		return () => {
+			clearTimeout(timeout)
+		}
+	}, [searchTerm])
+
 	return (
 		<div className="page">
-			<CardList books={othersBooks} />
-			<button onClick={handlePress} className="floatingButton">+</button>
+			<Input label="Busca" value={searchTerm} onChange={handleChange} />
+			<CardList books={booksToShow} showIcon />
 		</div>
 	);
 }
